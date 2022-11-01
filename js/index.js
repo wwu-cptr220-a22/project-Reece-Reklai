5089c2954b1c9c79bff0b8caeca663cc1e6013b2
REECE P REKLAI <official.reklai@gmail.com>
2022-10-31 23:17:51 -0700

    linting fixes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 // Get the modal
-var login = document.getElementById('index-login-container');
+const login = document.getElementById('index-login-container')
 
 // When the user clicks anywhere outside of the login, close it
-window.onclick = function(event) {
-    if (event.target == login) {
-        login.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target === login) {
+      login.style.display = 'none'
+  }
 }
 
 // Click on the login navigation will open the login form
-var userLogin = document.getElementById('user-login')
-userLogin.addEventListener('click', function(event){
-    const button = document.getElementById('index-login-container')
-    button.style.display='block'
-    button.style.width='auto'
+const userLogin = document.getElementById('user-login')
+userLogin.addEventListener('click', function () {
+  const button = document.getElementById('index-login-container')
+  button.style.display ='block'
+  button.style.width ='auto'
 })
