5c587d5d13204a72cd01364548aef92d52afbe9c
thomca <catherine.thomsen@wallawalla.edu>
2022-11-07 14:07:32 -0800

    setting up weather

--- js/weather.js
+++ js/weather.js
@@ -15,72 +15,31 @@ function renderError(error) {
 
 function renderSearchResults(results) {
     // get forecast from HTML page from weather report
+    const header = document.createElement('p')
+    const forecastParagraph = document.createElement('p')
+    header.textContent = 'CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS'
+    console.log(header)
     const forecast = parseForecast(results)
-
-    console.log(results)
-
     const weather = document.querySelector('#weather') // weather div
-
+    forecastParagraph.textContent = forecast
+    weather.appendChild(header)
+    weather.appendChild(forecastParagraph)
 }
 
-{/* <pre class="glossaryProduct">000
-ASPQ40 PGUM 071910
-RWRPQ
-WEATHER ROUNDUP FOR THE MARIANAS AND MICRONESIA
-NATIONAL WEATHER SERVICE TIYAN GU
-500 AM CHST TUE NOV 08 2022
-
-GUZ001&gt;004-072000-
-MARIANA ISLANDS-
-
-CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
-GUAM AIRPORT   CLEAR     81  77  89 E6        29.85R
-ANDERSEN AFB   PTCLDY    79  75  89 E12       29.86R
-ROTA AIRPORT   NOT AVBL
-TINIAN AP      NOT AVBL
-SAIPAN AP      PTCLDY    81  79  94 NE7       29.86S
-
-$$
-
-PMZ161-072000-
-REPUBLIC OF PALAU-
-
-CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
-KOROR AP       NOT AVBL
-KOROR WSO      NOT AVBL
-
-$$
-
-PMZ171&gt;174-072000-
-FEDERATED STATES OF MICRONESIA-
-
-CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
-YAP AIRPORT    MOCLDY    81  79  94 CALM      29.78S
-CHUUK AP       CLOUDY    79  75  89 VRB3      29.79R
-POHNPEI AP     NOT AVBL
-POHNPEI WSO    CLOUDY    75  75 100 CALM      29.84R
-KOSRAE AP      NOT AVBL
-
-$$
-
-PMZ181-072000-
-REPUBLIC OF THE MARSHALL ISLANDS-
-
-CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
-KWAJALEIN AP   CLEAR     82  75  78 E15       29.82R
-MAJURO AP      NOT AVBL
-MAJURO WSO     PTCLDY    82  75  78 E6        29.79R
-
-$$
-
-
-
-</pre> */}
-
 function parseForecast(weatherPage){
-    const pattern = /(?<=<pre class="glossaryProduct">)\d*\.\d*/
-    const WeatherDetails = weatherPage.match(pattern)
-    console.log(WeatherDetails)
+    const startRX = /pre class="glossaryProduct"/
+    const kororRX = /KOROR AP/
+    const kororEndRX = /KOROR WSO/
+    let startIndex = weatherPage.match(startRX).index
+    let endIndex
+    // arbitrary cutoff to contain all relivant data
+    const workingData = weatherPage.substring(startIndex, startIndex + 1300) 
+
+    startIndex = workingData.match(kororRX).index
+    endIndex = workingData.match(kororEndRX).index
+    console.log(startIndex, endIndex)
+    console.log(workingData.substring(startIndex, endIndex))
+    return workingData.substring(startIndex, endIndex)
 }
 
 fetchForecast()
