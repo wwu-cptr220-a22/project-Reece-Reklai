1c0e448c849b69a64a8f94ee8a5fef8f10aa98db
thomca <catherine.thomsen@wallawalla.edu>
2022-11-09 14:53:31 -0800

    updating emailing function

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-const submit = document.querySelector('#submit-button')
-
-document.querySelector('#contact-form').addEventListener('submit', event => {
-    event.preventDefault()
-    // adding event to let user know their form was submited
-    submit.value = "Message Sent!"
-    setTimeout(() => { submit.value = "Submit" }, 5000)
-})
