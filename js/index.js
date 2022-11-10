c42948ae6b88df2e5229fb034a4208dbb5a06b43
thomca <catherine.thomsen@wallawalla.edu>
2022-11-09 22:32:24 -0800

    working on linter

--- js/index.js
+++ js/index.js
@@ -1,32 +1,17 @@
-// Initialize and add the map (Buy Page)
-function initMap () {
-  // The location of Palau
-  // const palau = { lat: 7.53644, lng: 134.58159 }
-  let position // declared for each map based on hidden div for lat and lng
-  const latRX = /(?<=lat: )\d*\.\d*/
-  const lngRX = /(?<=lng: )\d*\.\d*/
+// Get the modal (Index Page)
+const login = document.getElementById('index-login-container')
 
-  // The map, centered at Palau
-  const locations = document.querySelectorAll('.google-maps')
-
-  locations.forEach(element => {
-    const positionData = element.children.item(0).textContent
-    const newLat = Number(positionData.match(latRX))
-    const newLng = Number(positionData.match(lngRX))
-
-    // console.log(newLat, newLng)
-
-    position = { lat: newLat, lng: newLng }
-
-    const map = new google.maps.Map(element, {
-      zoom: 11,
-      center: position
-    })
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position,
-      map
-    })
-  })
+// When the user clicks anywhere outside of the login, close it
+window.onclick = function (event) {
+  if (event.target === login) {
+    login.style.display = 'none'
+  }
 }
-window.initMap = initMap
+
+// Click on the login navigation will open the login form
+const userLogin = document.getElementById('user-login')
+userLogin.addEventListener('click', function () {
+  const button = document.getElementById('index-login-container')
+  button.style.display = 'block'
+  button.style.width = 'auto'
+})
