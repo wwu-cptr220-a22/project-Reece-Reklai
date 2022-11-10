b6c9f42948b34d077b6de43c30d262ca8b89f98f
b3nruby <benrubyramirez@gmail.com>
2022-11-09 19:52:46 -0800

    checking for linters

--- js/header.js
+++ js/header.js
@@ -1,5 +1,8 @@
 // header navbar
 class Header extends HTMLElement {
+  constructor () {
+    super()
+  }
   connectedCallback () {
     this.innerHTML = `
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
