65d0f9fcf2af266074ce594a07dd54256216be5b
thomca <catherine.thomsen@wallawalla.edu>
2022-11-13 13:40:29 -0800

    working with mail

--- js/mail.js
+++ js/mail.js
@@ -1,6 +1,10 @@
+'use strict'
+import {mailjet} from '../node_modules/node-mailjet'
+
 function sendEmail() {
-    const mailjet = require('node-mailjet')
-        .connect('****************************1234', '****************************abcd')
+    // const mailjet = require('node-mailjet')
+    mailjet
+    .connect('****************************1234', '****************************abcd')
     const request = mailjet
         .post("send", { 'version': 'v3.1' })
         .request({
@@ -26,6 +30,10 @@ function sendEmail() {
     request
         .then((result) => {
             console.log(result.body)
+            const submit = document.querySelector('#submit-button')
+            // adding event to let user know their form was submitted
+            submit.value = "Message Sent!"
+            setTimeout(() => { submit.value = "Submit" }, 5000)
         })
         .catch((err) => {
             console.log(err.statusCode)
@@ -36,3 +44,5 @@ document.querySelector('#contact-form').addEventListener('submit', event => {
     event.preventDefault()
     sendEmail()
 })
+
+sendEmail()
