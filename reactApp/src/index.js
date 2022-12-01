f716c08bf83ea0e8f576fc7b91c43477e4273124
thomca <catherine.thomsen@wallawalla.edu>
2022-11-30 21:11:09 -0800

    setting up example from class

---
+++ reactApp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+import SAMPLE_DOGS from './dogs.json' // a sample list of dogs (model)
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App pets={SAMPLE_DOGS} />)
