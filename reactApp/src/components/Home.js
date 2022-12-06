1f84bb278ba50228c65cb9879b8a697180159d5d
REECE P REKLAI <official.reklai@gmail.com>
2022-12-06 15:55:25 -0800

    weather works

--- reactApp/src/components/Home.js
+++ reactApp/src/components/Home.js
@@ -1,41 +1,16 @@
 import React, { Component } from "react"; // import React Component
 import "../css/stylesheet.css";
+import FetchForest from './Weather'
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.weatherPage =
-      "https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0";
     this.weatherTitle = "CITY\tSKY/WX\tTMP\tDP\tRH\tWIND\tPRESSURE\tREMARKS";
-  }
-
-  fetchForecast() {
-    const promise = fetch(this.weatherPage)
-      .then((response) => {
-        return response.text();
-      })
-      .then(this.renderForecast)
-      .catch((error) => {
-        console.log(error);
-      });
-    return promise;
-  }
-
-  renderForecast(results) {
-    let forecast = this.parseForecast(results);
-    let startRX = /pre class="glossaryProduct"/;
-    let kororRX = /KOROR AP/;
-    let kororEndRX = /KOROR WSO/;
-    let startIndex = forecast.match(startRX).index;
-    let workingData = forecast.substring(startIndex, startIndex + 1300);
-    startIndex = workingData.match(kororRX).index;
-    let endIndex = workingData.match(kororEndRX).index;
-    return workingData.substring(startIndex, endIndex);
   }
 
   render() {
     return (
-      <div>
+      <React.Fragment>
         <main id="carousel" aria-label="carousel">
           <div
             id="carousel-example-dark"
@@ -43,7 +18,7 @@ class Home extends Component {
             data-bs-ride="carousel"
           >
             <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="7500">
+              <div className="carousel-item active" data-bs-interval="5000">
                 <img
                   src="../img/palau-background/resort.jpg"
                   className="d-block w-100"
@@ -52,7 +27,7 @@ class Home extends Component {
                 <div className="carousel-caption d-md-block">
                   <div className="weather-status">
                     <h1>Palau Weather Status</h1>
-                    <div className="weather">{this.weatherTitle} <br/> {'reece'}</div>
+                    <div className="weather">{this.weatherTitle} <br/> {<FetchForest/>}</div>
                     <p>
                       Data from{" "}
                       <a href="https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0">
@@ -62,7 +37,7 @@ class Home extends Component {
                   </div>
                 </div>
               </div>
-              <div className="carousel-item" data-bs-interval="7500">
+              <div className="carousel-item" data-bs-interval="5000">
                 <img
                   src="../img/palau-background/palau-beach.jpg"
                   className="d-block w-100"
@@ -71,7 +46,7 @@ class Home extends Component {
                 <div className="carousel-caption d-block">
                   <div className="weather-status">
                     <h1>Palau Weather Status</h1>
-                    <div className="weather">{this.weatherTitle}</div>
+                    <div className="weather">{this.weatherTitle} <br/> {<FetchForest/>}</div>
                     <p>
                       Data from{" "}
                       <a href="https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0">
@@ -81,7 +56,7 @@ class Home extends Component {
                   </div>
                 </div>
               </div>
-              <div className="carousel-item" data-bs-interval="7500">
+              <div className="carousel-item" data-bs-interval="5000">
                 <img
                   src="../img/palau-background/ocean.jpg"
                   className="d-block w-100"
@@ -90,7 +65,7 @@ class Home extends Component {
                 <div className="carousel-caption d-md-block">
                   <div className="weather-status">
                     <h1>Palau Weather Status</h1>
-                    <div className="weather">{this.weatherTitle}</div>
+                    <div className="weather">{this.weatherTitle} <br/> {<FetchForest/>}</div>
                     <p>
                       Data from{" "}
                       <a href="https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0">
@@ -103,7 +78,7 @@ class Home extends Component {
             </div>
           </div>
         </main>
-      </div>
+      </React.Fragment>
     );
   }
 }
