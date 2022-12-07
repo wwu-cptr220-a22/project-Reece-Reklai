316b97a716783e93d09ae8ff25b987d55104bd28
thomca <catherine.thomsen@wallawalla.edu>
2022-12-06 20:45:58 -0800

    about stuff

--- reactApp/src/components/About.js
+++ reactApp/src/components/About.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react"; // import React Component
+import { Email } from "https://smtpjs.com/v3/smtp.js";
 import "../css/stylesheet.css";
 import "../css/about-style.css";
+
+// https://reactgo.com/react-get-input-value/
 
 class About extends Component {
   constructor(props) {
     super(props);
-    this.mailService = "https://smtpjs.com/v3/smtp.js";
+    // this.mailService = "https://smtpjs.com/v3/smtp.js";
+    this.state = { name: "", email: "", message: "", submitMessage: "Submit" }
   }
+
+  handleName = event => {
+    this.setState({ name: event.target.value });
+  }
+  handleEmail = event => {
+    this.setState({ email: event.target.value });
+  }
+  handleMessage = event => {
+    this.setState({ message: event.target.value });
+  }
+
   HandleSubmit = () => {
+    // tutorial found on https://www.youtube.com/watch?v=MV-Aqkjju64&t
+    Email.send({
+      SecureToken: '5b52722e-1196-4b73-9b5d-86718fac6991', // gitleaks:allow
+      To: 'PalauRealEstateWWU@gmail.com',
+      From: 'PalauRealEstateWWU@gmail.com',
+      Subject: 'Email Submission (' + this.state.name + ')',
+      Body: 'Name: ' + this.state.name + '<br>Email: ' + this.state.email + '<br>' + this.state.message
+    })
+      .then(message => {
+        console.log('send status: ' + message)
+        // adding event to let user know their form was submitted
+        this.setState({ name: "", email: "", message: "", submitMessage: "Message Sent!" });
+        setTimeout(() => { this.setState({submitMessage:"Submit"}) }, 5000)
+      })
+      .catch(er => {
+        console.log(er)
+      });
 
   }
   render() {
@@ -47,16 +79,16 @@ class About extends Component {
               <h3 class="about-form-title">Question and Help</h3>
               <form id="contact-form" onSubmit={this.HandleSubmit}>
                 <label class="about-name" for="name">Name</label>
-                <input type="text" id="name" value="" />
+                <input type="text" id="name" onChange={this.handleName} value={this.state.name} />
                 <br />
                 <label class="about-email" for="email">Email</label>
-                <input type="email" id="email" value="" />
+                <input type="email" id="email" onChange={this.handleEmail} value={this.state.email} />
                 <br />
                 <label class="about-message" for="message-box">Message</label>
                 <br />
-                <textarea name="name" id="message-box" rows="10" cols="30"></textarea>
+                <textarea name="name" id="message-box" rows="10" cols="30" onChange={this.handleMessage} value={this.state.message}></textarea>
                 <br />
-                <input type="submit" id="submit-button" name="final-message" />
+                <input type="submit" id="submit-button" name="final-message" value={this.state.submitMessage} />
               </form>
             </div>
           </div>
