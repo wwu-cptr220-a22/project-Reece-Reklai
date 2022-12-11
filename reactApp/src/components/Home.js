7b84be55934eadb6649e2d5fd01573ee0613b310
REECE P REKLAI <official.reklai@gmail.com>
2022-12-11 13:37:21 -0800

    weather works

--- reactApp/src/components/Home.js
+++ reactApp/src/components/Home.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react' // import React Component
-import FetchForest from './Weather'
 
 class Home extends Component {
   constructor (props) {
     super(props)
     this.weatherTitle = 'CITY\tSKY/WX\tTMP\tDP\tRH\tWIND\tPRESSURE\tREMARKS'
+    this.state = {
+      weather: ''
+    }
+  }
+
+  fetchForecast () {
+    const weatherPage = 'https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0'
+    const promise = fetch(weatherPage)
+      .then(response => {
+        return response.text()
+      }).then(renderSearchResults)
+      .catch((error) => { console.log(error) })
+  
+    function renderSearchResults (results) {
+      return parseForecast(results)
+    }
+  
+    function parseForecast (weatherPage) {
+      const startRX = /pre class="glossaryProduct"/
+      const kororRX = /KOROR AP/
+      const kororEndRX = /KOROR WSO/
+      let startIndex = weatherPage.match(startRX).index
+    
+      // arbitrary cutoff to contain all relivant data
+      const workingData = weatherPage.substring(startIndex, startIndex + 1300)
+      startIndex = workingData.match(kororRX).index
+      const endIndex = workingData.match(kororEndRX).index
+      return workingData.substring(startIndex, endIndex)
+    }
+    promise.then(value => {this.setState({weather:value})})
+    return this.state.weather
   }
 
   render () {
@@ -26,7 +56,7 @@ class Home extends Component {
                 <div className='carousel-caption d-md-block'>
                   <div className='weather-status'>
                     <h1>Palau Weather Status</h1>
-                    <div className='weather'>{this.weatherTitle} <br /> <FetchForest /></div>
+                    <div className='weather'>{this.weatherTitle} <br />{this.fetchForecast()}</div>
                     <p>
                       Data from{' '}
                       <a href='https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0'>
@@ -45,7 +75,7 @@ class Home extends Component {
                 <div className='carousel-caption d-block'>
                   <div className='weather-status'>
                     <h1>Palau Weather Status</h1>
-                    <div className='weather'>{this.weatherTitle} <br /> <FetchForest /></div>
+                    <div className='weather'>{this.weatherTitle} <br /> {this.fetchForecast()}</div>
                     <p>
                       Data from{' '}
                       <a href='https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0'>
@@ -64,7 +94,7 @@ class Home extends Component {
                 <div className='carousel-caption d-md-block'>
                   <div className='weather-status'>
                     <h1>Palau Weather Status</h1>
-                    <div className='weather'>{this.weatherTitle} <br /> <FetchForest /></div>
+                    <div className='weather'>{this.weatherTitle} <br />{this.fetchForecast()}</div>
                     <p>
                       Data from{' '}
                       <a href='https://forecast.weather.gov/product.php?site=GUM&issuedby=PQ&product=RWR&format=CI&version=1&glossary=0'>
