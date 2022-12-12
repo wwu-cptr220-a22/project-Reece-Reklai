448a9cb9c1f21a3b5684d20b988e355c93ac1243
REECE P REKLAI <official.reklai@gmail.com>
2022-12-12 15:18:08 -0800

    google map works

--- reactApp/src/components/Listings.js
+++ reactApp/src/components/Listings.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react' // import React Component
-
-class Listings extends Component {
-  render () {
-    return (
-      <div />
-    )
+import React, { useState, useEffect } from 'react';
+import GMap from './GMap'
+ 
+// API key of the google map
+const GOOGLE_MAP_API_KEY = 'AIzaSyCIvBhGsNo2E_QcMZgnuZw4wZ-eh0fVDcY';
+ 
+// load google map script
+const loadGoogleMapScript = (callback) => {
+  if (typeof window.google === 'object' && typeof window.google.maps === 'object') {
+    callback();
+  } else {
+    const googleMapScript = document.createElement("script");
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCIvBhGsNo2E_QcMZgnuZw4wZ-eh0fVDcY`;
+    window.document.body.appendChild(googleMapScript);
+    googleMapScript.addEventListener("load", callback);
   }
 }
-
-export default Listings
+ 
+const Listings = () => {
+  const [loadMap, setLoadMap] = useState(false);
+ 
+  useEffect(() => {
+    loadGoogleMapScript(() => {
+      setLoadMap(true)
+    });
+  }, []);
+ 
+  return (
+    <div className="App">
+      <a href="https://www.cluemediator.com">Clue Mediator</a><br /><br />
+      {!loadMap ? <div>Loading...</div> : <GMap />}
+    </div>
+  );
+}
+ 
+export default Listings;
